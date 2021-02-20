1d5c8df15b7cd1275cd9f7a0a53c899504dd1082
aHowe <awhasuka@foxmail.com>
2021-02-20 17:25:15 +0800

    设置标头

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog</h1>")
 	} else if r.URL.Path == "/about" {
